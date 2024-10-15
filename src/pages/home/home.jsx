90e94ed86951b1757c392cc604aeda8caee66106
zaidturman <zaidturman17@gmail.com>
2024-10-15 17:51:09 +0300

    first commit

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "../../components/ui/header/header";
+import NavBar from "../../components/ui/navbar/navbar";
+
+function Home () {
+    return( 
+  <>
+        <Header/>
+        <NavBar/>
+  </>
+    
+);
+}
+
+export default Home;
